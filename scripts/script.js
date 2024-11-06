f706c2167b7ba1396d716bf0b4b7c22cef29ba7b
Moe-Karim <mohamadkarimfahs@gmail.com>
2024-11-06 08:31:36 +0200

    Added: animated about section,Removed sticky nav

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,27 @@
-const navbar = document.getElementById("nav-bar");
-let sticky = navbar.offsetTop;
-window.onscroll = ()=> {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("navigation");
-  } else {
-    navbar.classList.remove("navigation");
-  }
-};
+document.addEventListener("DOMContentLoaded", (event) => {
 
-gsap.from(".pop", {
+    function popMove(){
+document.getElementById("hero").addEventListener('mouseenter',()=>{ gsap.to(".pop", {
     duration: 2,
     x: 100,
   });
+});
+}
 
+function aboutMove(){
+gsap.registerPlugin(TextPlugin);
+const about=document.getElementById("about");
+about.addEventListener('mouseenter',()=>{
+  gsap.to("#about", {
+    duration: 30,
+    text: {
+      value: "Based in the heart of Beirut,this digital marketing startup brings fresh ideas and innovative strategies to help brands make meaningful connections with their audiences. With a focus on authenticity and impact, every campaign is crafted to engage and inspire,ensuring that each message truly resonates<br><br>Driven by a commitment to excellence,the team offers a wide range of services from digital strategy to content creation,all tailored to meet the unique needs of each business. The goal is simple: to transform visions into dynamic,successful digital experiences that leave a lasting impression.",
+      delimiter: "",
+      newClass: "about",
+    },
+  });
+});
+}
 
   function scrollToElement(target, e) {
     const elem = target ? document.querySelector(target) : null;
@@ -39,3 +48,7 @@ gsap.from(".pop", {
   if (window.location.hash) {
     scrollToElement(window.location.hash);
   }
+  popMove();
+aboutMove();
+});
+
